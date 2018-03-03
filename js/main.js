47eecd858a2010a6e3b4e6487864170aecfccb8c
justmarried <saurodip.halder11@yahoo.com>
2018-03-03 23:12:50 +0530

    upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$("header").load("html/navigation.html");
+	$("#home").load("html/home.html");
+	$("#about").load("html/about.html");
+	$("#team").load("html/team.html");
+	$("#services").load("html/services.html");
+	$("#work").load("html/work.html");
+	$("#process").load("html/process.html");
+	$("#skills").load("html/skills.html");
+	$("#contact").load("html/contact.html");
+	$("footer").load("html/footer.html");
+});
