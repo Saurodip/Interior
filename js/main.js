55cf07e91a88b3f77fd2db02cbcc87b0a28d59bf
justmarried <saurodip.halder11@yahoo.com>
2018-03-04 10:23:13 +0530

    upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 	$("#home").load("html/home.html");
 	$("#about").load("html/about.html");
 	$("#team").load("html/team.html");
-	$("#services").load("html/services.html");
 	$("#work").load("html/work.html");
 	$("#process").load("html/process.html");
 	$("#skills").load("html/skills.html");
